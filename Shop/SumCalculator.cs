945686efc3cde0d86b22d69ee12cf19ca95dd17b
bm295 <baominh.nguyen.295@gmail.com>
2020-04-22 16:24:58 +0700

    Add test and refactor ApplyDiscount

--- Shop/SumCalculator.cs
+++ Shop/SumCalculator.cs
@@ -56,27 +56,38 @@ namespace Shop
             return sumOfCost;
         }
 
-        public decimal Calculate(decimal amount, int type, int years)
+        public decimal ApplyDiscount(decimal price, AccountStatus accountStatus, int timeOfHavingAccountInYears)
         {
-            decimal result = 0;
-            decimal disc = (years > 5) ? (decimal)5 / 100 : (decimal)years / 100;
-            if (type == 1)
+            decimal priceAfterDiscount = 0;
+            decimal discountForLoyaltyInPercentage = (timeOfHavingAccountInYears > 5) ? (decimal)5 / 100 : (decimal)timeOfHavingAccountInYears / 100;
+            switch (accountStatus)
             {
-                result = amount;
+                case AccountStatus.NotRegistered:
+                    priceAfterDiscount = price;
+                    break;
+                case AccountStatus.SimpleCustomer:
+                    priceAfterDiscount = price - 0.1m * price;
+                    priceAfterDiscount = priceAfterDiscount - discountForLoyaltyInPercentage * priceAfterDiscount;
+                    break;
+                case AccountStatus.ValuableCustomer:
+                    priceAfterDiscount = 0.7m * price;
+                    priceAfterDiscount = priceAfterDiscount - discountForLoyaltyInPercentage * priceAfterDiscount;
+                    break;
+                case AccountStatus.MostValuableCustomer:
+                    priceAfterDiscount = price - 0.5m * price;
+                    priceAfterDiscount = priceAfterDiscount - discountForLoyaltyInPercentage * priceAfterDiscount;
+                    break;
             }
-            else if (type == 2)
-            {
-                result = (amount - (0.1m * amount)) - disc * (amount - (0.1m * amount));
-            }
-            else if (type == 3)
-            {
-                result = (0.7m * amount) - disc * (0.7m * amount);
-            }
-            else if (type == 4)
-            {
-                result = (amount - (0.5m * amount)) - disc * (amount - (0.5m * amount));
-            }
-            return result;
+            return priceAfterDiscount;
+        }
+
+        public enum AccountStatus
+        {
+            Unknown = 0,
+            NotRegistered = 1,
+            SimpleCustomer = 2,
+            ValuableCustomer = 3,
+            MostValuableCustomer = 4
         }
     }
 }
--- UnitTest/SumCalculatorTests.cs
+++ UnitTest/SumCalculatorTests.cs
@@ -2,20 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using Xunit;
+using static Shop.SumCalculator;
 
 namespace UnitTest
 {
     public class SumCalculatorTests
     {
+        private SumCalculator _sumCalculator = new SumCalculator();
+
         [Fact]
         public void SumTest_If_Group_3_Products_And_Option_Cost_Should_Sum_Of_Cost_Of_Each_Group()
         {
-            var sumCalculator = new SumCalculator();
             var numberOfProducts = 3;
             var optionToSum = "cost";
             var expected = new List<int> {6, 15, 24, 21};
             
-            var actual = sumCalculator.Sum(numberOfProducts, optionToSum).ToList();
+            var actual = _sumCalculator.Sum(numberOfProducts, optionToSum).ToList();
 
             Assert.Equal(expected, actual);
         }
@@ -23,13 +25,29 @@ namespace UnitTest
         [Fact]
         public void SumTest_If_Group_4_Products_And_Option_Revenue_Should_Sum_Of_Revenue_Of_Each_Group()
         {
-            var sumCalculator = new SumCalculator();
             var numberOfProducts = 4;
             var optionToSum = "revenue";
             var expected = new List<int> { 50, 66, 60 };
 
-            var actual = sumCalculator.Sum(numberOfProducts, optionToSum).ToList();
+            var actual = _sumCalculator.Sum(numberOfProducts, optionToSum).ToList();
 
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(1000, AccountStatus.Unknown, 0, 0)]
+        [InlineData(1000, AccountStatus.Unknown, 6, 0)]
+        [InlineData(1000, AccountStatus.NotRegistered, 0, 1000)]
+        [InlineData(1000, AccountStatus.NotRegistered, 6, 1000)]
+        [InlineData(1000, AccountStatus.SimpleCustomer, 0, 900)]
+        [InlineData(1000, AccountStatus.SimpleCustomer, 6, 855)]
+        [InlineData(1000, AccountStatus.ValuableCustomer, 0, 700)]
+        [InlineData(1000, AccountStatus.ValuableCustomer, 6, 665)]
+        [InlineData(1000, AccountStatus.MostValuableCustomer, 0, 500)]
+        [InlineData(1000, AccountStatus.MostValuableCustomer, 6, 475)]
+        public void CalculateTest(decimal price, AccountStatus accountStatus, int timeOfHavingAccountInYears, decimal expected)
+        {
+            var actual = _sumCalculator.ApplyDiscount(price, accountStatus, timeOfHavingAccountInYears);
             Assert.Equal(expected, actual);
         }
     }
