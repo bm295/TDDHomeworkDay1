e1e26fbdfbfc17c9d8503f2d13045331352053a1
bm295 <baominh.nguyen.295@gmail.com>
2020-04-24 09:46:13 +0700

    Apply general rule for discount

--- Shop/SumCalculator.cs
+++ Shop/SumCalculator.cs
@@ -71,7 +71,7 @@ namespace Shop
                     priceAfterDiscount = priceAfterDiscount - discountForLoyaltyInPercentage * priceAfterDiscount;
                     break;
                 case AccountStatus.ValuableCustomer:
-                    priceAfterDiscount = 0.7m * price;
+                    priceAfterDiscount = price - 0.3m * price;
                     priceAfterDiscount = priceAfterDiscount - discountForLoyaltyInPercentage * priceAfterDiscount;
                     break;
                 case AccountStatus.MostValuableCustomer:
